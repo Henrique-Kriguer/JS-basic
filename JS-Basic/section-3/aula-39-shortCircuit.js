a3d76a0e517799a08923fca55eba43420ca391b4
henrique-kriguer <henriquekriguer@gmail.com>
2023-07-03 16:18:11 -0300

    criação do modelo

--- JS-Basic/section-3/aula-39-shortCircuit.js
+++ JS-Basic/section-3/aula-39-shortCircuit.js
@@ -7,3 +7,6 @@ const d = false;
 const e = NaN;
 
 console.log( a || b|| c || d || e);
+// retorna o unico valor true das variáveis, pois a string com a palavra false não é valor false !
+// se estivesse sem aspas, retornaria o ultimo elemento falos, no caso NaN;
+
