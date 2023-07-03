cdbb4b38267fb76786e5493fb056886e668765d7
henrique-kriguer <henriquekriguer@gmail.com>
2023-07-03 14:39:06 -0300

    correção pastas e estudo lógica

---
+++ JS-Basic/section-4/23-IIFE.js
@@ -0,0 +1,9 @@
+(function() {
+    const nome = 'Henrique';
+    console.log(nome);
+})();
+
+const nome= 'Qualquer coisa';
+
+// perceba que a variável nome não foi sobrescrita pelo escopo lexico,
+// porque a função anonima envolvida por parenteses é imeditamente invocada com () 
